ed3ef4dcf96527b87d33fb4d327a9f3bddaa0bf5
Gowtham1729 <chitipolu.gowtham@iitgn.ac.in>
2017-01-15 20:05:57 +0530

    upload history database php

---
+++ create_table.php
@@ -0,0 +1,30 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "gowtham";
+$dbname = "History";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE History (
+id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(100) NOT NULL,
+email VARCHAR(50) NOT NULL,
+filename VARCHAR(500) NOT NULL,
+upload_time TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table History created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
